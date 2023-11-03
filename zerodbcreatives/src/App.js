9947cd055de8e370924830b9c323968c9e3c79cd
k1au3 <lindquistconcern@gmail.com>
2023-11-03 18:06:29 +0300

    Linkage

--- zerodbcreatives/src/App.js
+++ zerodbcreatives/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Hero from '../components/hero';
+import About from '../components/aboutUS';
+import LargeIMG from '../components/largeIMG';
+import Footer from '../components/footer';
+import '../styles/app.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero />
+      <About />
+      <LargeIMG />
+      <Footer />
     </div>
   );
 }
