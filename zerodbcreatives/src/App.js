027c65906e43919843d7a51b71c42150b89121a1
Sherry680 <faithnjeri506@gmail.com>
2023-11-11 22:51:56 +0300

    add changes

--- zerodbcreatives/src/App.js
+++ zerodbcreatives/src/App.js
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 import Hero from './components/hero';
 import About from './components/aboutUS';
 import LargeIMG from './components/largeIMG';
 import Footer from './components/footer';
+=======
+import Hero from '../components/hero.js';
+import About from '../components/aboutUS.js';
+import LargeIMG from '../components/largeIMG.js';
+import Footer from '../components/footer.js';
+>>>>>>> 1b8863337a5c9667a5084b35e2ba9fc2300b225c
 import '../styles/app.css';
 
 function App() {
